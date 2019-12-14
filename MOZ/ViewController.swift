16a18fa95748485343ad92875d58c36ded294db5
gracia-experience <ninonanoni2038@gmail.com>
2019-12-14 23:57:01 +0900

    Initial Commit

---
+++ MOZ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MOZ
+//
+//  Created by 二宮啓 on 2019/12/14.
+//  Copyright © 2019 二宮啓. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
